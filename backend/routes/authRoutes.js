651990b1f4640fe7bee988ec9b7911f2a370b99a
Akashkapoor0 <akashkapoor.work@gmail.com>
2026-04-17 22:57:52 +0530

    Delete backend directory

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { registerAdmin, adminLogin } from "../controllers/authController.js";
-
-const router = express.Router();
-
-router.post("/admin-register", registerAdmin);
-router.post("/admin-login", adminLogin);
-
-export default router;
